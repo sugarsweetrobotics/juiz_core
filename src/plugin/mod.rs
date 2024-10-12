7c5c6578f6b860ca83f038e6bf10ac8821967484
Yuki Suga <ysuga@sugarsweetrobotics.com>
2024-10-12 12:24:08 +0900

    pluginぱっけーじのmodを回収

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -4,11 +4,8 @@
 mod plugin;
 mod python;
 mod cpp;
-pub mod rust;
+mod rust;
 
-// pub use rust_plugin::RustPlugin;
-// pub use python_plugin::PythonPlugin;
 pub use plugin::{Plugin, JuizObjectPlugin, concat_dirname, plugin_name_to_file_name};
-// pub use python_plugin::{pyany_to_mat, pyany_to_value, get_entry_point, get_python_function_signature, check_object_is_ndarray, capsulemap_to_pytuple, python_process_call};
 pub use rust::{ProcessFactoryImpl, ContainerFactoryImpl, ContainerProcessFactoryImpl};
 pub(crate) use rust::RustPlugin;
