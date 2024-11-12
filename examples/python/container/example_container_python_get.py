253f5bf9b4d6a1ec0ec00b4383b27fed2935ca1d
Yuki Suga <ysuga@sugarsweetrobotics.com>
2024-11-12 12:41:35 +0900

    READMEをアップデートして、サンプルを微修正

--- examples/python/container/example_container_python_get.py
+++ examples/python/container/example_container_python_get.py
@@ -1,10 +1,4 @@
-from juiz.juiz import ProcessManifest
 from juiz import juiz_container_process
-
-# def manifest():
-#     return ProcessManifest("example_container_python_get")\
-#         .set_container_type("example_container_python")\
-#         .into_value()
 
 @juiz_container_process(
     container_type="example_container_python"
@@ -12,6 +6,3 @@ from juiz import juiz_container_process
 def example_container_python_get(container):
     # print(f'example_container_python_get({container}) called')
     return container.value
-
-# def container_process_factory():
-#     return manifest(), example_container_python_get
--- examples/rust/container/example_container_get/src/lib.rs
+++ examples/rust/container/example_container_get/src/lib.rs
@@ -4,6 +4,7 @@ use example_container::ExampleContainer;
 
 #[juiz_container_process(container_type = "example_container")]
 fn example_container_get(container: &mut ContainerImpl<ExampleContainer>) -> JuizResult<Capsule> {
+    println!("example_container_get() called");
     return Ok(jvalue!(container.value).into());
 }
 
