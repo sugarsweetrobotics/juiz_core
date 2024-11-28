338305b24c3b9b927a0ef3e6b592b456c678e803
Yuki Suga <ysuga@sugarsweetrobotics.com>
2024-11-28 11:34:41 +0900

    update version number

--- juiz_macro/src/process/gen_process_factory.rs
+++ juiz_macro/src/process/gen_process_factory.rs
@@ -9,7 +9,7 @@ pub(crate) fn factory_tokenstream(function_ident: syn::Ident) -> proc_macro::Tok
         #[no_mangle]
         pub unsafe extern "Rust" fn process_factory() -> JuizResult<ProcessFactoryStruct> {
             env_logger::init();
-            Ok(juiz_sdk::factory::process_factory(manifest(), #function_ident))
+            Ok(juiz_sdk::prelude::process_factory(manifest(), #function_ident))
         }
     }.into()
 }
@@ -25,7 +25,7 @@ pub(crate) fn component_factory_tokenstream(function_ident: syn::Ident, factory_
         #[no_mangle]
         pub unsafe extern "Rust" fn #factory_name_ident() -> JuizResult<ProcessFactoryStruct> {
             // env_logger::init();
-            Ok(juiz_sdk::factory::process_factory(#manifest_function_name_ident(), #function_ident))
+            Ok(juiz_sdk::prelude::process_factory(#manifest_function_name_ident(), #function_ident))
         }
     }.into()
 }
